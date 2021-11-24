81eec55a14f18da4a528789366177423938dfc8f
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2021-11-24 19:15:38 +0200

    set default 5x5 grid

--- hammerspoon/grid.lua
+++ hammerspoon/grid.lua
@@ -1,4 +1,4 @@
-hs.grid.setGrid'3x3'
+hs.grid.setGrid'5x5'
 hs.grid.setMargins("0,0")
 hs.window.animationDuration = 0
 
@@ -22,6 +22,7 @@ hs.hotkey.bind(mash, "l", function() hs.grid.resizeWindowWider() end)
 hs.hotkey.bind(mash, "2", function() hs.grid.setGrid('2x2'); hs.alert.show('Grid set to 2x2'); end)
 hs.hotkey.bind(mash, "3", function() hs.grid.setGrid('3x3'); hs.alert.show('Grid set to 3x3'); end)
 hs.hotkey.bind(mash, "4", function() hs.grid.setGrid('4x4'); hs.alert.show('Grid set to 4x4'); end)
+hs.hotkey.bind(mash, "5", function() hs.grid.setGrid('5x5'); hs.alert.show('Grid set to 5x5'); end)
 
 --- /: move window to next screen
 hs.hotkey.bind(mash, "/", function() local win = getWin(); win:moveToScreen(win:screen():next()) end)
